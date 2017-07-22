03826802f96a6e78848f44bc8a2cddecc7909be7
daedalus418 <morin.gaelle@gmail.com>
2017-07-22 22:59:18 +0200

    Content file added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Content from './components/Content/Content';
 import Footer from './components/Footer/Footer';
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
         return (
             <div className="App">
                 <Navbar />
+                <Content />
                 <Footer />
             </div>
         );

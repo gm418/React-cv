786e7e74b832cef800f187726e99aaff9c83c0b6
daedalus418 <morin.gaelle@gmail.com>
2017-07-28 15:35:10 +0200

    footer set

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return (
         <div className="App">
-            Footer
+            <footer className="footer">
+                <div className="container">
+                    <div className="content has-text-centered">
+                        <p>
+                            <a className="icon" target="_blank" href="https://github.com/Daedalus418/">
+                                <i className="fa fa-github"></i>
+                            </a>
+                            <a className="icon" target="_blank" href="https://www.linkedin.com/in/gaelle-morin/">
+                                <i className="fa fa-linkedin"></i>
+                            </a>
+                            <a className="icon" target="_blank" href="https://twitter.com/daedalus418">
+                                <i className="fa fa-twitter"></i>
+                            </a>
+                        </p>
+                        <p>
+                            © Gaëlle Morin 2017
+                        </p>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 };

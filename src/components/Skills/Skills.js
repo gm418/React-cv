ff91c338c26960782520980f04ab3c54684370a9
daedalus418 <morin.gaelle@gmail.com>
2017-08-10 12:37:44 +0200

    more flexibilty added for mobile devices

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -9,32 +9,40 @@ class Skills extends Component {
       <section className="hero is-large is-danger">
         <div className="container">
           <div className="notification is-primary">
+
             <div className="hero-head">
               <h2 className="title">Skills</h2>
             </div>
+
             <div className="hero-body">
               <div className="columns">
-
-                <div className="column is-half">
+                <div className="column">
                   <h3>Devlopment</h3>
-                  <div className="notification is-info">
-                    <div className="hexagon">HTML5</div>
+                  <div className="columns is-mobile">
+                    <div className="column">
+                      <div className="notification is-info">
+                        <div className="hexagon">HTML5</div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-
                 <div className="column">
-                    <h3>Tools</h3>
-                    <ul className="notification is-danger">
-                      <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Git/Github</li>
-                      <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Atom</li>
-                      <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Command line</li>
-                      <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Chrome DevTools</li>
-                      <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Scrum/Kanban</li>
-                    </ul>
+                  <h3>Tools</h3>
+                  <div className="columns is-mobile">
+                    <div className="column">
+                      <ul className="notification is-danger">
+                        <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Git/Github</li>
+                        <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Atom</li>
+                        <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Command line</li>
+                        <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Chrome DevTools</li>
+                        <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Scrum/Kanban</li>
+                      </ul>
+                    </div>
+                  </div>
                 </div>
-
               </div>
             </div>
+
           </div>
         </div>
       </section>

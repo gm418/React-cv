b4f93c1a318b1307d9e704caa903765d5208a662
daedalus418 <morin.gaelle@gmail.com>
2017-08-11 17:15:18 +0200

    Finally hexagones work! Still improving

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Skills.css'
 
 class Skills extends Component {
@@ -6,22 +6,63 @@ class Skills extends Component {
   render() {
 
     return (
-      <section className="hero is-large is-danger">
+      <section className="hero is-medium is-danger is-bold">
         <div className="container">
-          <div className="notification is-primary">
 
-            <div className="hero-head">
+          <div className="hero-body">
+            <div className="container">
               <h2 className="title">Skills</h2>
-            </div>
-
-            <div className="hero-body">
               <div className="columns">
                 <div className="column">
                   <h3>Devlopment</h3>
                   <div className="columns is-mobile">
                     <div className="column">
-                      <div className="notification is-info">
-                        <div className="hexagon">HTML5</div>
+                      <div className="notification is-info hexResponsive">
+                        <ul className="hexGrid">
+                          <li className="hex">
+                            <div className="hexIn">
+                              <span className="hexLink">
+                                <h4>HTML5</h4>
+                              </span>
+                            </div>
+                          </li>
+                          <li className="hex">
+                            <div className="hexIn">
+                              <span className="hexLink">
+                                <h4>CSS3</h4>
+                              </span>
+                            </div>
+                          </li>
+                          <li className="hex">
+                            <div className="hexIn">
+                              <span className="hexLink">
+                                <h4>JS</h4>
+                              </span>
+                            </div>
+                          </li>
+                          <li className="hex">
+                            <div className="hexIn">
+                              <span className="hexLink">
+                                <h4>AngularJS</h4>
+                              </span>
+                            </div>
+                          </li>
+                          <li className="hex">
+                            <div className="hexIn">
+                              <span className="hexLink">
+                                <h4>ReactJS</h4>
+                              </span>
+                            </div>
+                          </li>
+                          <li className="hex">
+                            <div className="hexIn">
+                              <span className="hexLink">
+                                <h4>Sass</h4>
+                              </span>
+                            </div>
+                          </li>
+
+                        </ul>
                       </div>
                     </div>
                   </div>
@@ -30,20 +71,35 @@ class Skills extends Component {
                   <h3>Tools</h3>
                   <div className="columns is-mobile">
                     <div className="column">
-                      <ul className="notification is-danger">
-                        <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Git/Github</li>
-                        <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Atom</li>
-                        <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Command line</li>
-                        <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Chrome DevTools</li>
-                        <li><span className="icon"><i className="fa fa-angle-right" aria-hidden="true"></i></span>Scrum/Kanban</li>
+                      <ul className="notification is-success">
+                        <li>
+                          <span className="icon">
+                            <i className="fa fa-angle-right" aria-hidden="true"></i>
+                          </span>Git/Github</li>
+                        <li>
+                          <span className="icon">
+                            <i className="fa fa-angle-right" aria-hidden="true"></i>
+                          </span>Atom</li>
+                        <li>
+                          <span className="icon">
+                            <i className="fa fa-angle-right" aria-hidden="true"></i>
+                          </span>Command line</li>
+                        <li>
+                          <span className="icon">
+                            <i className="fa fa-angle-right" aria-hidden="true"></i>
+                          </span>Chrome DevTools</li>
+                        <li>
+                          <span className="icon">
+                            <i className="fa fa-angle-right" aria-hidden="true"></i>
+                          </span>Scrum/Kanban</li>
                       </ul>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-
           </div>
+
         </div>
       </section>
 

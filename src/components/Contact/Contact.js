a6dd415cc597f2395128b81ee2e2465d269efd6f
daedalus418 <morin.gaelle@gmail.com>
2017-08-24 11:37:45 +0200

    error messages ok

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -34,15 +34,15 @@ class Contact extends Component {
     switch(fieldName) {
       case 'name':
         nameValid = value.length >= 1;
-        fieldValidationErrors.name = nameValid ? '' : 'champs vide';
+        fieldValidationErrors.name = nameValid ? '' : 'Ce champs ne peut être vide.';
         break;
       case '_replyto':
         _replytoValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-        fieldValidationErrors._replyto = _replytoValid ? '' : ' is invalid';
+        fieldValidationErrors._replyto = _replytoValid ? '' : 'Cet email est invalide.';
         break;
       case 'message':
         messageValid = value.length >= 1;
-        fieldValidationErrors.message = messageValid ? '' : 'champs vide';
+        fieldValidationErrors.message = messageValid ? '' : 'Ce champs ne peut être vide.';
         break;
       default:
         break;
@@ -58,6 +58,10 @@ class Contact extends Component {
     this.setState({formValid: this.state.nameValid && this.state._replytoValid && this.state.messageValid});
   }
 
+  errorClass(error) {
+    return(error.length === 0 ? '' : 'is-danger');
+  }
+
   render() {
 
     return (
@@ -69,10 +73,6 @@ class Contact extends Component {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in commodo purus. Etiam ac consectetur eros. Morbi ac leo non est consequat egestas.</p>
           </div>
 
-          <div className="notification">
-            <FormErrors formErrors={this.state.formErrors} />
-          </div>
-
           <div className="notification is-warning">
             <form className="columns" action="https://formspree.io/morin.gaelle@gmail.com" method="POST">
 
@@ -80,38 +80,32 @@ class Contact extends Component {
                 <div className="field">
                   <label className="label" htmlFor="name">Nom</label>
                   <div className="control">
-                    <input className="input" type="text" name="name" placeholder="" value={this.state.name} onChange={(event) => this.handleUserInput(event)}></input>
+                    <input className={`input ${this.errorClass(this.state.formErrors.name)}`}  type="text" name="name" placeholder="" value={this.state.name} onChange={(event) => this.handleUserInput(event)}></input>
                   </div>
                 </div>
 
                 <div className="field">
                   <label className="label" htmlFor="email">Email</label>
-                  <div className="control has-icons-right">
-                    <input className="input is-danger" type="text" name="_replyto" placeholder="" value={this.state._replyto} onChange={(event) => this.handleUserInput(event)}></input>
-                    <span className="icon is-small is-right">
-                      <i className="fa fa-warning"></i>
-                    </span>
+                  <div className="control">
+                    <input className={`input ${this.errorClass(this.state.formErrors._replyto)}`}  type="text" name="_replyto" placeholder="" value={this.state._replyto} onChange={(event) => this.handleUserInput(event)}></input>
                   </div>
-                  <p className="help is-danger">This email is invalid</p>
                 </div>
 
                 <div className="field">
                   <label className="label" htmlFor="subject">Sujet</label>
-                  <div className="control has-icons-right">
-                    <input className="input is-success" type="text" name="_subject" placeholder=""></input>
-                    <span className="icon is-small is-right">
-                      <i className="fa fa-check"></i>
-                    </span>
+                  <div className="control">
+                    <input className="input" type="text" name="_subject" placeholder=""></input>
                   </div>
-                  <p className="help is-success">This username is available</p>
                 </div>
 
                 <div className="field">
                   <label className="label" htmlFor="message">Message</label>
                   <div className="control">
-                    <textarea className="textarea" name="message" placeholder="" value={this.state.message} onChange={(event) => this.handleUserInput(event)}></textarea>
+                    <textarea className={`textarea ${this.errorClass(this.state.formErrors.message)}`}  name="message" placeholder="" value={this.state.message} onChange={(event) => this.handleUserInput(event)}></textarea>
                   </div>
                 </div>
+
+                <FormErrors formErrors={this.state.formErrors} />
 
                 <input type="hidden" name="_language" value="fr" />
 

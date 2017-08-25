5d3105423d0ab971758a9e42fd04c5733662803d
daedalus418 <morin.gaelle@gmail.com>
2017-08-25 12:13:30 +0200

    portfolio page is set

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Content from './components/Content/Content';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Experience from './components/Experience/Experience';
+import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 
@@ -19,6 +20,7 @@ class App extends Component {
                 <About />
                 <Skills />
                 <Experience />
+                <Portfolio />
                 <Contact />
                 <Footer />
             </div>
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import './Portfolio.css';
+
+
+class Portfolio extends Component {
+
+  render() {
+
+    return (
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Mon travail</h1>
+          <article className="hero is-success">
+            <div className="hero-body">
+              <figure className="image is-128x128">
+                <img src="http://bulma.io/images/placeholders/128x128.png" alt="me"></img>
+              </figure>
+              <h2 className="title">Lorem</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <div className="tags">
+                <span className="tag">One</span>
+                <span className="tag">Two</span>
+                <span className="tag">Three</span>
+              </div>
+            </div>
+          </article>
+          <article className="hero is-danger">
+            <div className="hero-body">
+              <figure className="image is-128x128">
+                <img src="http://bulma.io/images/placeholders/128x128.png" alt="me"></img>
+              </figure>
+              <h2 className="title">Lorem</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <div className="tags">
+                <span className="tag">One</span>
+                <span className="tag">Two</span>
+                <span className="tag">Three</span>
+              </div>
+            </div>
+          </article>
+          <article className="hero is-warning">
+            <div className="hero-body">
+              <figure className="image is-128x128">
+                <img src="http://bulma.io/images/placeholders/128x128.png" alt="me"></img>
+              </figure>
+              <h2 className="title">Lorem</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <div className="tags">
+                <span className="tag">One</span>
+                <span className="tag">Two</span>
+                <span className="tag">Three</span>
+              </div>
+            </div>
+          </article>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
